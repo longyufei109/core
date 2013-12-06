4f7207b0cad277bfd19c478f1dff07c42b3613a8
subdigit <subdigit@gmail.com>
2013-12-05 16:44:34 -0800

    Refactored out acronymity from non acronymity packages

--- play/PlayInterceptor.java
+++ play/PlayInterceptor.java
@@ -1,12 +1,9 @@
 package com.subdigit.data.play;
 
-import org.apache.http.HttpStatus;
-
 import play.api.templates.Html;
 import play.mvc.Call;
 import play.mvc.Result;
 
-import com.subdigit.acronymity.data.models.User;
 import com.subdigit.broker.RequestResponseBroker;
 import com.subdigit.broker.play.PlayRequestResponseBroker;
 import com.subdigit.data.BaseInterceptor;
@@ -17,87 +14,21 @@ import com.subdigit.data.BasicAccessor;
 import com.subdigit.request.WebRequest;
 import com.subdigit.request.WebRequest.Action;
 import com.subdigit.request.play.PlayRequest;
-import com.subdigit.result.BasicResult.StatusLevel;
-import com.subdigit.result.play.PlayResult;
-import com.subdigit.result.ServiceResult;
 import com.subdigit.result.WebResult;
+import com.subdigit.result.play.PlayResult;
 
 
-public abstract class PlayInterceptor<T extends BaseModel<K>, K, X extends BasicAccessor<T, K>, Y extends BaseManager<User, T, K, X>, Z extends BaseService<User, T, K, X, Y>> extends BaseInterceptor<User, T, K, X, Y, Z, Result, Html, Call>
+public abstract class PlayInterceptor<UserType, T extends BaseModel<K>, K, X extends BasicAccessor<T, K>, Y extends BaseManager<UserType, T, K, X>, Z extends BaseService<UserType, T, K, X, Y>> extends BaseInterceptor<UserType, T, K, X, Y, Z, Result, Html, Call>
 {
 	@Override
 	protected RequestResponseBroker<?, ?, ?> getNewBroker(){ return getNewPlayBroker(); }
 	protected abstract PlayRequestResponseBroker getNewPlayBroker();
 
 	@Override
-	protected WebResult<T, Result, Html, Call> getNewResult(WebRequest<User> request){ return getNewPlayResult((PlayRequest) request); }
-	protected abstract PlayResult<T> getNewPlayResult(PlayRequest request);
+	protected WebResult<T, Result, Html, Call> getNewResult(WebRequest<UserType> request){ return getNewPlayResult((PlayRequest<UserType>) request); }
+	protected abstract PlayResult<T> getNewPlayResult(PlayRequest<UserType> request);
 
 	@Override
-	protected WebRequest<User> getNewRequest(RequestResponseBroker<?, ?, ?> broker, Action action){ return (WebRequest<User>) getNewPlayRequest((PlayRequestResponseBroker) broker, action); }
-	protected abstract PlayRequest getNewPlayRequest(PlayRequestResponseBroker broker, Action action);
-
-
-	@Override protected boolean isAllowedToRetrieve(ServiceResult<T> result, User user){ return true; }
-	@Override protected boolean isAllowedToCreate(ServiceResult<T> result, User user)
-	{
-		if(user == null){
-			result.setSuccess(false);
-			result.setStatusCode(HttpStatus.SC_UNAUTHORIZED);
-			result.addStatus(HttpStatus.SC_UNAUTHORIZED, "You must login before being allowed to create.", StatusLevel.FATAL);
-			
-			return false;
-		}
-
-		return true;
-	}
-
-	@Override protected boolean isAllowedToUpdate(ServiceResult<T> result, User user, T model)
-	{
-		if(user == null){
-			result.setSuccess(false);
-			result.setStatusCode(HttpStatus.SC_UNAUTHORIZED);
-			result.addStatus(HttpStatus.SC_UNAUTHORIZED, "You must login before being allowed to update.", StatusLevel.FATAL);
-			
-			return false;
-
-		} else if(user.isAdmin()){
-			return true;
-
-		} else if(!isCreator(user, model)){
-			result.setSuccess(false);
-			result.setStatusCode(HttpStatus.SC_UNAUTHORIZED);
-			result.addStatus(HttpStatus.SC_UNAUTHORIZED, "You are not authorized to update!", StatusLevel.FATAL);
-			
-			return false;
-		}
-
-		return true;
-	}
-
-	@Override protected boolean isAllowedToDelete(ServiceResult<T> result, User user, T model)
-	{
-		if(user == null){
-			result.setSuccess(false);
-			result.setStatusCode(HttpStatus.SC_UNAUTHORIZED);
-			result.addStatus(HttpStatus.SC_UNAUTHORIZED, "You must login before being allowed to delete.", StatusLevel.FATAL);
-			
-			return false;
-
-		} else if(user.isAdmin()){
-			return true;
-
-		} else if(!isCreator(user, model)){
-			result.setSuccess(false);
-			result.setStatusCode(HttpStatus.SC_UNAUTHORIZED);
-			result.addStatus(HttpStatus.SC_UNAUTHORIZED, "You are not authorized to delete!", StatusLevel.FATAL);
-
-			return false;
-		}
-
-		return true;
-	}
-
-	protected abstract boolean isCreator(User user, T model);
-	protected abstract T setCreateOverrides(User user, T model);
+	protected WebRequest<UserType> getNewRequest(RequestResponseBroker<?, ?, ?> broker, Action action){ return getNewPlayRequest((PlayRequestResponseBroker) broker, action); }
+	protected abstract PlayRequest<UserType> getNewPlayRequest(PlayRequestResponseBroker broker, Action action);
 }
